90dfa721c411b7ed756019bc28f2a8267755f591
Tomi Leminen <tleminen@student.uef.fi>
2025-01-15 02:39:47 +0200

    egg clicker

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   //kuvien vaihtaminen toiminto
 
-  //Haettiin nappien elementit
+  //Haetaan nappien elementit
   const slider = document.querySelector(".slider");
   const edellinenNappi = document.getElementById("edellinen");
   const seuraavaNappi = document.getElementById("seuraava");
@@ -67,4 +67,59 @@ window.addEventListener("DOMContentLoaded", (event) => {
   //Lisätään tpahtumakuuntelijat
   edellinenNappi.addEventListener("click", () => vaihdaKuva("edellinen"));
   seuraavaNappi.addEventListener("click", () => vaihdaKuva("seuraava"));
+
+  //MUNA CLICKER
+
+  let munia = 0;
+  let klikkauksenTeho = 1;
+  let taso = 50;
+  let voittoRaja = 30000;
+  const muna = document.getElementById("muna");
+  const laskuri = document.getElementById("laskuri");
+  const upgradeButton = document.getElementById("upgrade");
+
+  // Värit, joita muna käyttää
+  const varit = ["#000000", "#CD7F32", "#c0c0c0", " #ffd700"];
+  let variIndeksi = 0; // Alkuväri
+
+  //Lisätään tapahtumakuuntelija
+  muna.addEventListener("click", () => {
+    munia += klikkauksenTeho;
+    laskuri.textContent = munia;
+
+    // Tarkistetaan, onko saavutettu 5000 munan raja
+    //console.log(munia);
+    if (munia > taso) {
+      taso *= 2;
+      variIndeksi = (variIndeksi + 1) % varit.length; // Vaihdetaan väriä
+      //console.log("uudestaan: ", variIndeksi);
+      muna.style.backgroundColor = varit[variIndeksi];
+    }
+
+    // Tarkistetaan, onko pelaaja voittanut pelin
+    if (munia >= voittoRaja) {
+      alert("Voitit pelin!");
+      // Asetetaan muuttujat alkuarvoihin
+      munia = 0;
+      klikkauksenTeho = 1;
+      klikkauksia = 0;
+      variIndeksi = 0;
+      muna.style.backgroundColor = varit[variIndeksi];
+      laskuri.textContent = munia;
+      upgradeButton.textContent = `Upgrade (${1 + klikkauksenTeho}x)`;
+    }
+  });
+
+  //Lisätään tapahtumakuuntelija
+
+  upgradeButton.addEventListener("click", () => {
+    if (munia >= 10) {
+      munia -= 10;
+      klikkauksenTeho++;
+      laskuri.textContent = munia;
+      upgradeButton.textContent = `Upgrade (${1 + klikkauksenTeho}x)`;
+    }
+  });
+
+
 });
